babfa643c36c642b4b35431ffb9880f99d66052b
mathieuway91 <mathieu.way91@gmail.com>
2019-11-07 11:55:11 +0100

    remove debug

--- PistoGang/Assets/Scripts/PlayerController.cs
+++ PistoGang/Assets/Scripts/PlayerController.cs
@@ -11,24 +11,26 @@ public class PlayerController
 
     public PlayerController(UduinoDevice controller)
     {
+        //INIT CONTROLLER
         SetUduinoDevice(controller);
+
+        //INIT BUTTON
         UduinoManager.Instance.pinMode(controller, 2, PinMode.Input_pullup);
         UduinoManager.Instance.pinMode(controller, 3, PinMode.Input_pullup);
         UduinoManager.Instance.pinMode(controller, 4, PinMode.Input_pullup);
         UduinoManager.Instance.pinMode(controller, 5, PinMode.Input_pullup);
-        //INIT LED
-        UduinoManager.Instance.pinMode(controller, 14, PinMode.PWM);
-        UduinoManager.Instance.pinMode(controller, 15, PinMode.PWM);
-        UduinoManager.Instance.pinMode(controller, 16, PinMode.PWM);
-        UduinoManager.Instance.pinMode(controller, 17, PinMode.PWM);
         buttonLastFrameArray[0] = 1;
         buttonLastFrameArray[1] = 1;
         buttonLastFrameArray[2] = 1;
         buttonLastFrameArray[3] = 1;
+
+        //INIT LED
+        UduinoManager.Instance.pinMode(controller, 14, PinMode.PWM);
+        UduinoManager.Instance.pinMode(controller, 15, PinMode.PWM);
+        UduinoManager.Instance.pinMode(controller, 16, PinMode.PWM);
         UduinoManager.Instance.analogWrite(controller, 14, 255);
         UduinoManager.Instance.analogWrite(controller, 15, 255);
         UduinoManager.Instance.analogWrite(controller, 16, 255);
-        UduinoManager.Instance.analogWrite(controller, 17, 255);
     }
 
     // Update is called once per frame
@@ -44,12 +46,7 @@ public class PlayerController
             else
                 buttonDown[i] = false;
             buttonLastFrameArray[i] = value;
-            if(buttonLastFrameArray[i] == 0)
-                UduinoManager.Instance.analogWrite(controller, i + 14, 255);
-            else
-                UduinoManager.Instance.analogWrite(controller, i + 14, 0);
         }
-
     }
 
     public void SetUduinoDevice(UduinoDevice controller)
